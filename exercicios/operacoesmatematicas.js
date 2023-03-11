5d9732442ee52128796318ac8badde85d24307aa
MurakamiJuliana <murakamijuliana@hotmail.com>
2023-03-11 17:32:21 -0300

    Exercício sobre as operações matemáticas (Soma, Subtração, Multiplicação, Divisão, Potenciação e Radiciação) em JavaScript

---
+++ exercicios/operacoesmatematicas.js
@@ -0,0 +1,56 @@
+// OPERAÇÕES MATEMÁTICAS
+ 
+    console.log("FUNÇÕES MATEMÁTICAS")
+
+// SOMA ============================================================
+
+    const funcaoSoma = (a, b) =>{
+        return a + b
+    }
+
+    console.log("SOMA: ", funcaoSoma(2, 4))
+
+
+// SUBTRAÇÃO ========================================================
+
+    const funcaoSubtracao = (a, b) =>{
+        return a - b
+    }
+
+    console.log("SUBTRAÇÃO: ", funcaoSubtracao(10, 5))
+
+
+// MULTIPLICAÇÃO ====================================================
+
+    const funcaoMultiplicacao = (a, b) =>{
+        return a * b
+    }
+
+    console.log("MULTIPLICAÇÃO: ", funcaoMultiplicacao(2, 5)) 
+
+
+// DIVISÃO =========================================================
+
+    const funcaoDivisao = (a, b) =>{
+        return a / b
+    }
+
+    console.log("DIVISÃO: ", funcaoDivisao(6, 2))
+
+
+// POTENCIAÇÃO =====================================================
+
+    const funcaoPotenciacao = (a, b) =>{
+        return Math.pow(a, b)
+    }
+
+    console.log("POTENCIAÇÃO: ", funcaoPotenciacao(3, 2))
+
+
+// RADICIAÇÃO ======================================================
+
+    const funcaoRadiciacao = (a, b) =>{
+        return Math.sqrt(a)
+    }
+
+    console.log("RADICIAÇÃO:", funcaoRadiciacao(25))
